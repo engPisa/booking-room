3c77820c56076cb1d1d3e6a26eb632af0814f86f
devPisa <cesar.pisa@alunos.bpkedu.com.br>
2025-06-16 04:26:38 -0300

    Update: Script sql

---
+++ room-service/src/main/resources/db/migration/V1__Create_Room_Table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE room (
+    id UUID PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    capacity INT NOT NULL,
+
+    CONSTRAINT capacity_must_be_positive CHECK (capacity > 0)
+);
